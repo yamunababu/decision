cb6b31141587bb6b3e835ae2369c2e4fcdfd8532
yamunababu <71624972+yamunababu@users.noreply.github.com>
2020-10-31 09:51:23 +0530

    created dec.cpp
    this file prints the voting eligibility of a person

---
+++ dec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout<<"Enter Age of a person:";
+	cin>>age;
+	if(age>=18)
+	{
+		cout<<"\n person is eligible for voting";
+	}
+	else
+		cout<<"\n person is not eligible for voting";
+return 0;
+}
